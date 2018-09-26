e5318fcc0b3f954c7bcbcd9bdd3dfea447d673fd
Soren ZVand <sunrise.freedom@gmail.com>
2018-09-26 14:28:59 +0330

    add db-context

--- src/Mobin.CoreProject.Config/Bootstrapper.cs
+++ src/Mobin.CoreProject.Config/Bootstrapper.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Mobin.CoreProject.Config
 {
     public static class Bootstrapper
     {
+        public static void ConfigDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+
+        }
     }
 }
---
+++ src/Mobin.CoreProject.Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Mobin.CoreProject.Core.Entities;
+
+namespace Mobin.CoreProject.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
