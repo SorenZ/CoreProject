5fbc7bc9f3e715e7013f777b43e18caee693b360
Mojtaba Dashtinejad <mojtaba@gmail.com>
2018-10-01 15:15:16 +0330

    fixed IPaginated result

--- src/Mobin.CoreProject.Core/ServiceContracts/IForestService.cs
+++ src/Mobin.CoreProject.Core/ServiceContracts/IForestService.cs
@@ -1,5 +1,6 @@
 ﻿using Alamut.Data.Paging;
 using Alamut.Data.Service;
+using Mobin.CoreProject.Core.DTOs.Forest;
 using Mobin.CoreProject.Core.Entities;
 using Mobin.CoreProject.Core.SearchCriteria.Forest;
 
@@ -7,6 +8,6 @@ namespace Mobin.CoreProject.Core.ServiceContracts
 {
     public interface IForestService : ICrudService<Forest>
     {
-        IPaginated<Forest> GetData(ForestGetDataSC criteria, int page);
+        IPaginated<ForestSummaryDTO> GetData(ForestGetDataSC criteria, int page);
     }
 }
--- src/Mobin.CoreProject.Service/AppServices/ForestService.cs
+++ src/Mobin.CoreProject.Service/AppServices/ForestService.cs
@@ -8,6 +8,8 @@ using Alamut.Data.Repository;
 using Alamut.Data.Service;
 using Alamut.Helpers.Linq;
 using Alamut.Service;
+using AutoMapper.QueryableExtensions;
+using Mobin.CoreProject.Core.DTOs.Forest;
 using Mobin.CoreProject.Core.Entities;
 using Mobin.CoreProject.Core.SearchCriteria.Forest;
 using Mobin.CoreProject.Core.ServiceContracts;
@@ -24,13 +26,15 @@ namespace Mobin.CoreProject.Service.AppServices
             _repository = repository;
         }
 
-        public IPaginated<Forest> GetData(ForestGetDataSC criteria, int page = 1)
+        public IPaginated<ForestSummaryDTO> GetData(ForestGetDataSC criteria, int page = 1)
         {
             var model = _repository
                 .Queryable
 
-                .WhereIf(string.IsNullOrWhiteSpace(criteria.Title),
+                .WhereIf(! string.IsNullOrWhiteSpace(criteria.Title),
                     q => q.Title.Contains(criteria.Title))
+
+                .ProjectTo<ForestSummaryDTO>()
 
                 .ToPaginated(new PaginatedCriteria(page));
 
