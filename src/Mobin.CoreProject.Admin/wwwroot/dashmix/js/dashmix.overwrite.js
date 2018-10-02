08b5bbca13b4edb98f34312634aa7578f310484a
Mojtaba Dashtinejad <mojtaba@gmail.com>
2018-10-02 12:52:45 +0330

    disable submit button on form submit

--- src/Mobin.CoreProject.Admin/wwwroot/dashmix/js/dashmix.overwrite.js
+++ src/Mobin.CoreProject.Admin/wwwroot/dashmix/js/dashmix.overwrite.js
@@ -21,35 +21,45 @@
     });
 }
 
-$(function () {
-    $('[data-role="delete"]').on('click', function (e) {
-        $('.tooltip').remove();
+$(function() {
+    $('[data-role="delete"]').on('click',
+        function(e) {
+            $('.tooltip').remove();
 
-        var shouldContinue = confirm('آیا از حذف مطمئن هستید؟');
+            var shouldContinue = confirm('آیا از حذف مطمئن هستید؟');
 
-        if (!shouldContinue) {
+            if (!shouldContinue) {
+                return false;
+            }
+
+            var $elem = $(this);
+            var $row = $elem.closest('tr');
+            var url = $elem.attr('href');
+            $.get(url, { isAjax: true })
+                .then(function(response) {
+                    if (response.Succeed) {
+                        $row.css({
+                            'pointer-events': 'none',
+                            'opacity': 0.3
+                        });
+                        $elem.tooltip('disable');
+                    }
+
+                    showBeautyMessage(response);
+                })
+                .fail(function(response) {
+                    showBeautyMessage(response);
+                });
+
             return false;
-        }
+        });
+});
 
-        var $elem = $(this);
-        var $row = $elem.closest('tr');
-        var url = $elem.attr('href');
-        $.get(url, { isAjax: true })
-            .then(function (response) {
-                if (response.Succeed) {
-                    $row.css({
-                        'pointer-events': 'none',
-                        'opacity': 0.3
-                    });
-                    $elem.tooltip('disable');
-                }
-
-                showBeautyMessage(response);
-            })
-            .fail(function (response) {
-                showBeautyMessage(response);
-            });
-
-        return false;
+$(function() {
+    $('form').one('submit', function (e) {
+        var $form = $(this);
+        setTimeout(function () {
+            $form.find(':submit').prop('disabled', true);
+        }, 0);
     });
-})
+});
