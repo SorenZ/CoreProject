413dea2becba9d509cd61c9301496266aa86a410
Mojtaba Dashtinejad <mojtaba@gmail.com>
2018-10-08 14:37:30 +0330

    introduce PluginHelper.PersianDateTimePicker

---
+++ src/Mobin.CoreProject.Admin/Helper/PluginHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Alamut.Data.Paging;
+using DNTPersianUtils.Core;
+
+namespace Mobin.CoreProject.Admin.Helper
+{
+    public static class PluginHelper
+    {
+        public static string PersianDateTimePicker()
+        {
+            return @"
+                <script src='/dashmix/js/plugins/MD.BootstrapPersianDateTimePicker/dist/jquery.md.bootstrap.datetimepicker.js'></script>
+                <link rel='stylesheet' href='/dashmix/js/plugins/MD.BootstrapPersianDateTimePicker/dist/jquery.md.bootstrap.datetimepicker.style.css' />
+            ";
+        }
+    }
+}
