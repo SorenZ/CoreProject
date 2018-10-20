78be6aec28d822fc2bf9fb5508636291b3aaa07a
Mojtaba Dashtinejad <mojtaba@gmail.com>
2018-10-20 13:08:45 +0330

    introduce UserRole crud

---
+++ src/Mobin.CoreProject.Core/ServiceContracts/IUserRoleService.cs
@@ -0,0 +1,12 @@
+﻿using Alamut.Data.Paging;
+using Alamut.Data.Service;
+using Mobin.CoreProject.Core.DTOs.User;
+using Mobin.CoreProject.Core.Entities;
+
+namespace Mobin.CoreProject.Core.ServiceContracts
+{
+    public interface IUserRoleService : ICrudService<UserRole>
+    {
+        IPaginated<UserRole> GetData(int page, int size);
+    }
+}
---
+++ src/Mobin.CoreProject.Service/AppServices/UserRoleService.cs
@@ -0,0 +1,28 @@
+﻿using Alamut.Data.Linq;
+using Alamut.Data.Paging;
+using Alamut.Data.Repository;
+using Alamut.Service;
+using AutoMapper.QueryableExtensions;
+using Mobin.CoreProject.Core.Entities;
+using Mobin.CoreProject.Core.ServiceContracts;
+
+namespace Mobin.CoreProject.Service.AppServices
+{
+    public class UserRoleService : CrudService<UserRole>, IUserRoleService
+    {
+        public UserRoleService(IRepository<UserRole> repository) : base(repository)
+        {
+
+        }
+
+        public IPaginated<UserRole> GetData(int page = 1, int size = 10)
+        {
+            var model = this.Repository
+                .Queryable
+
+                .ToPaginated(new PaginatedCriteria(page, size));
+
+            return model;
+        }
+    }
+}
