ebf96fb2f2e49570df85cf876bcfde7ccd3430b8
Mojtaba Dashtinejad <mojtaba@gmail.com>
2018-10-20 12:40:11 +0330

    introduce User entity

---
+++ src/Mobin.CoreProject.Core/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Alamut.Data.Entity;
+
+namespace Mobin.CoreProject.Core.Entities
+{
+    public class User
+    {
+        [Key]
+        [MaxLength(50)]
+        public string UserName { get; set; }
+    }
+}
