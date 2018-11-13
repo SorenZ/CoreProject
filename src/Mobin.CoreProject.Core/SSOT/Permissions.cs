ac24b4047d732b0cc9a8fbc322678ef98fc501a5
Mojtaba Dashtinejad <mojtaba@gmail.com>
2018-11-13 16:05:22 +0330

    introduce Permissions enum SSOT

---
+++ src/Mobin.CoreProject.Core/SSOT/Permissions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Mobin.CoreProject.Core.SSOT
+{
+    /// <summary>
+    /// [provides permissions of system]
+    /// </summary>
+    public enum Permissions
+    {
+        ForestCreate,
+        ForestEdit,
+        ForestDelete,
+    }
+}
