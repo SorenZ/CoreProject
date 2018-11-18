492f0ca96390468866db80b89ff06317891f354a
Mojtaba Dashtinejad <mojtaba@gmail.com>
2018-11-18 09:32:38 +0330

    first try to implement Log page

---
+++ src/Mobin.CoreProject.Admin/Controllers/LogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mobin.CoreProject.Admin.Controllers
+{
+    public class LogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
