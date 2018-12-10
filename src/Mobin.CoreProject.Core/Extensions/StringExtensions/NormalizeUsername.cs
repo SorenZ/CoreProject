e0385945c884d840bdfe4fdead28ed8061e58451
Mojtaba Dashtinejad <mojtaba@gmail.com>
2018-12-10 08:17:05 +0330

    introduce NormalizeUsername

---
+++ src/Mobin.CoreProject.Core/Extensions/StringExtensions/NormalizeUsername.cs
@@ -0,0 +1,13 @@
+﻿namespace Mobin.CoreProject.Core.Extensions.StringExtensions
+{
+    public static partial class StringExtensions
+    {
+        public static string NormalizeUsername(this string str)
+        {
+            str = str.ToLower().Replace("mobinnet\\", "");
+            str = str.ToLower().Replace("@mobinnet.net", "");
+
+            return str;
+        }
+    }
+}
