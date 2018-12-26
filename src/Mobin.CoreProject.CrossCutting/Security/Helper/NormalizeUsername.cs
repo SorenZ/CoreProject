62129807245259fb428eeb52340fb79ee1264f36
Soren ZVand <sunrise.freedom@gmail.com>
2018-12-26 14:21:01 +0330

    move normalize user name to CrossCutting.Security.Helper

---
+++ src/Mobin.CoreProject.CrossCutting/Security/Helper/NormalizeUsername.cs
@@ -0,0 +1,13 @@
+﻿namespace Mobin.CoreProject.CrossCutting.Security.Helper
+{
+    public static partial class StringExtensions
+    {
+        public static string NormalizeUsername(this string str)
+        {
+            str = str.ToLower().Replace("mobinnet\\", "");
+            str = str.ToLower().Replace("@mobinnet.net", "");
+
+            return str;
+        }
+    }
+}
